8522d7537836fe8129494bd6b2d989ff0ce3bb19
Lomnyak Nabaala <lomnyaknab@gmail.com>
2025-09-19 14:36:42 +0300

    committed

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,11 +1,12 @@
 export interface PillProps {
   title: string;
+  styles: string;
 }
 
 export interface ButtonProps {
   title: string;
   styles: {
     size: 'small' | 'medium' | 'large';
-    shape: 'rounded-sm' | 'rounded-md' | 'rounded-full';
+    shape: 'rounded-sm' | 'rounded-md' | 'rounded-lg';
   };
 }
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -14,15 +14,15 @@ const Landing: React.FC = () => {
       <div className="grid grid-cols-3 gap-4">
         <Button title="Small Rounded" styles={{ size: 'small', shape: 'rounded-sm' }} />
         <Button title="Small Medium" styles={{ size: 'small', shape: 'rounded-md' }} />
-        <Button title="Small Full" styles={{ size: 'small', shape: 'rounded-full' }} />
+        <Button title="Small Full" styles={{ size: 'small', shape: 'rounded-lg' }} />
         <Button title="Medium Rounded" styles={{ size: 'medium', shape: 'rounded-sm' }} />
         <Button title="Medium Medium" styles={{ size: 'medium', shape: 'rounded-md' }} />
-        <Button title="Medium Full" styles={{ size: 'medium', shape: 'rounded-full' }} />
+        <Button title="Medium Full" styles={{ size: 'medium', shape: 'rounded-lg' }} />
         <Button title="Large Rounded" styles={{ size: 'large', shape: 'rounded-sm' }} />
         <Button title="Large Medium" styles={{ size: 'large', shape: 'rounded-md' }} />
-        <Button title="Large Full" styles={{ size: 'large', shape: 'rounded-full' }} />
+        <Button title="Large Full" styles={{ size: 'large', shape: 'rounded-lg' }} />
       </div>
-      <Button title="Get Started" styles={{ size: 'medium', shape: 'rounded-full' }} />
+      <Button title="Get Started" styles={{ size: 'medium', shape: 'rounded-lg' }} />
     </main>
   );
 };
