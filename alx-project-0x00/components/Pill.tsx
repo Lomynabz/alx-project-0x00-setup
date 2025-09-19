287005ab35dae4849bc51e51706d2db2c4e9e000
Lomnyak Nabaala <lomnyaknab@gmail.com>
2025-09-19 13:53:51 +0300

    committed

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,14 +1,11 @@
+import { PillProps } from "@/interfaces";
 import React from 'react';
 
-interface PillProps {
-  title: string;
-}
 
 const Pill: React.FC<PillProps> = ({ title }) => {
   return (
     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className=" text-sm ">Title</p>
-       {title}
+      <p className=" text-sm ">{title}</p>
     </div>
   )
 }
