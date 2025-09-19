d129fec076d52e3e946f7f53ff4cca292f6df8e0
Lomnyak Nabaala <lomnyaknab@gmail.com>
2025-09-19 14:40:22 +0300

    committed

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -14,15 +14,15 @@ const Landing: React.FC = () => {
       <div className="grid grid-cols-3 gap-4">
         <Button title="Small Rounded" styles={{ size: 'small', shape: 'rounded-sm' }} />
         <Button title="Small Medium" styles={{ size: 'small', shape: 'rounded-md' }} />
-        <Button title="Small Full" styles={{ size: 'small', shape: 'rounded-lg' }} />
+        <Button title="Small Full" styles={{ size: 'small', shape: 'rounded-Full' }} />
         <Button title="Medium Rounded" styles={{ size: 'medium', shape: 'rounded-sm' }} />
         <Button title="Medium Medium" styles={{ size: 'medium', shape: 'rounded-md' }} />
-        <Button title="Medium Full" styles={{ size: 'medium', shape: 'rounded-lg' }} />
+        <Button title="Medium Full" styles={{ size: 'medium', shape: 'rounded-Full' }} />
         <Button title="Large Rounded" styles={{ size: 'large', shape: 'rounded-sm' }} />
         <Button title="Large Medium" styles={{ size: 'large', shape: 'rounded-md' }} />
-        <Button title="Large Full" styles={{ size: 'large', shape: 'rounded-lg' }} />
+        <Button title="Large Full" styles={{ size: 'large', shape: 'rounded-Full' }} />
       </div>
-      <Button title="Get Started" styles={{ size: 'medium', shape: 'rounded-lg' }} />
+      <Button title="Get Started" styles={{ size: 'medium', shape: 'rounded-Full' }} />
     </main>
   );
 };
